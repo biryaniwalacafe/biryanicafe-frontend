42ccb3e30f9103cc88445a34086c58833b5882e3
biryaniwalacafe <biryaniwalacafe@gmail.com>
2026-01-30 22:08:11 +0530

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -75,7 +75,7 @@ export default function Profile() {
             <div className="flex items-center justify-between">
               <div>
                 <h1 className="font-serif text-5xl font-bold mb-2" data-testid="text-profile-name">
-                  {user?.username}
+                  {user?.name}
                 </h1>
                 <p className="text-xl text-muted-foreground" data-testid="text-profile-email">
                   {user?.email}
